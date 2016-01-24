3d7c9ab87ace4cf0d684555c447816b2b3917cb6
nishantmadu <nishant.madu@gmail.com>
2016-01-24 23:03:53 +0530

    Clicking on Delete should remove the last character from preview

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,9 @@ function handleInput(key){
   $('#preview').append(key);
 }
 
+function previewContent(){
+  return $('#preview').html();
+}
 
 
 $(document).ready(function(){
@@ -9,10 +12,16 @@ $(document).ready(function(){
       var key = $(this).text();
       if(key=="0")
       {
-        if($('#preview').html()!="0")
+        if(previewContent() !="0")
         {
           handleInput(key);
         }
+      }
+      else if (key== "DEL") {
+        var preview = previewContent();
+        var newPreview = preview.slice(0,-1);
+        $('#preview').html(newPreview);
+
       }
       else{
         handleInput(key);
